22758eaf86df1061a1da7f4932b74a71d42408f3
Milena Marković <116622019+MilenaM06@users.noreply.github.com>
2025-07-14 20:14:32 +0200

    Add UI Tests
    * Create dotnet-desktop.yml
    
    * Update dotnet-desktop.yml
    
    * Update dotnet-desktop.yml
    
    * Update dotnet-desktop.yml
    
    * add net framework project
    
    * Update dotnet-desktop.yml
    
    * add cli project
    
    * resolve conflicts
    
    * add cpp project
    
    * update ci-cd yml
    
    * update ci-cd yml
    
    * update ci-cd yml
    
    * update ci-cd yml
    
    * update ci-cd yml
    
    * add separate solutions
    
    * resolve conflicts
    
    * add dotnet and cpp native tests
    
    * update yml file
    
    * update wpf csproj
    
    * update yml file
    
    * update installer
    
    * update yml file
    
    * update yml file
    
    * update yml file
    
    * update yml file
    
    * update yml file
    
    * update yml file
    
    * update yml file
    
    * update yml file
    
    * update yml file
    
    * update yml file
    
    * update yml file
    
    * update yml file
    
    * update yml file
    
    * update yml file
    
    * update yml file
    
    * update yml file
    
    * Update ci-cd.yml
    
    * update yml file
    
    * update yml file
    
    * update yml file
    
    * update yml file
    
    * update yml file
    
    * update yml file
    
    * update yml file
    
    * update yml file
    
    * update .net framework .csproj
    
    * update yml file
    
    * update yml file
    
    * update yml file
    
    * update yml file
    
    * update yml file
    
    * add ui tests
    
    * update yml file
    
    * update yml file
    
    * update yml file
    
    * update yml file
    
    * update yml file
    
    * update yml file
    
    * update yml file
    
    * update ui tests
    
    * update ui tests
    
    * update ui tests

---
+++ DotNetSolution/Tests/UITests/UITests.cs
@@ -0,0 +1,111 @@
+using System.Diagnostics;
+using System.Windows.Automation;
+
+namespace UITests
+{
+    public class Tests
+    {
+        Process? appProcess;
+        AutomationElement mainWindow;
+
+        private static readonly string AppPath =
+            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "MultiLangApp", "WpfApp.exe");
+
+        private void WaitUntil(Func<bool> condition, int timeoutMs = 10000, int pollIntervalMs = 100)
+        {
+            var sw = Stopwatch.StartNew();
+            while (sw.ElapsedMilliseconds < timeoutMs)
+            {
+                if (condition())
+                    return;
+                Thread.Sleep(pollIntervalMs);
+            }
+            Assert.Fail("Condition was not met within timeout.");
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            appProcess = Process.Start(AppPath);
+            WaitUntil(() =>
+                AutomationElement.RootElement.FindFirst(
+                    TreeScope.Children,
+                    new PropertyCondition(AutomationElement.NameProperty, "Multi-Language App"))
+                != null, timeoutMs: 5000);
+
+            mainWindow = AutomationElement.RootElement.FindFirst(
+                TreeScope.Children,
+                new PropertyCondition(AutomationElement.NameProperty, "Multi-Language App"));
+
+            Assert.IsNotNull(mainWindow, "Main window not found");
+        }
+
+        private void ClickButtonAndCheckOutput(string buttonName, string expectedOutput)
+        {
+            var button = mainWindow.FindFirst(TreeScope.Descendants,
+                new PropertyCondition(AutomationElement.NameProperty, buttonName));
+            Assert.IsNotNull(button, $"Button '{buttonName}' not found");
+
+            var invokePattern = button.GetCurrentPattern(InvokePattern.Pattern) as InvokePattern;
+            Assert.IsNotNull(invokePattern, $"Button '{buttonName}' does not support InvokePattern");
+            invokePattern.Invoke();
+
+            var outputBox = mainWindow.FindFirst(TreeScope.Descendants,
+                new PropertyCondition(AutomationElement.AutomationIdProperty, "OutputTextBox"));
+            Assert.IsNotNull(outputBox, "OutputTextBox not found");
+
+            var valuePattern = outputBox.GetCurrentPattern(ValuePattern.Pattern) as ValuePattern;
+            Assert.That(valuePattern, Is.Not.Null, "OutputTextBox does not support ValuePattern");
+
+            WaitUntil(() => valuePattern.Current.Value == expectedOutput, timeoutMs: 10000);
+
+            Assert.That(valuePattern.Current.Value, Is.EqualTo(expectedOutput));
+        }
+
+        [Test]
+        public void Test_FortranButton()
+        {
+            ClickButtonAndCheckOutput("Fortran", "Hello there!");
+        }
+
+        [Test]
+        public void Test_CppNativeButton()
+        {
+            ClickButtonAndCheckOutput("C++ Native", "Hello from C++ Native Project!");
+        }
+
+        [Test]
+        public void Test_CliButton()
+        {
+            ClickButtonAndCheckOutput("C++/CLI", "Hello from C++/CLI Project!");
+        }
+
+        [Test]
+        public void Test_NetFrameworkButton()
+        {
+            ClickButtonAndCheckOutput(".NET Framework", "Hello from .NET Framework Project!");
+        }
+
+        [Test]
+        public void Test_NetStandardButton()
+        {
+            ClickButtonAndCheckOutput(".NET Standard", "Hello from .NET Standard Project!");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (appProcess != null)
+            {
+                if (!appProcess.HasExited)
+                {
+                    appProcess.Kill();
+                    appProcess.WaitForExit();
+                }
+                appProcess.Dispose();
+                appProcess = null;
+            }
+        }
+    }
+
+}
